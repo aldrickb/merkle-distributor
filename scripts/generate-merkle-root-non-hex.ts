39c27b5f949923a48e763930058b387200bad144
Aldrick Bonaobra <git@aldrickb.com>
2021-06-25 14:25:12 +0800

    Add generate-merkle-root-non-hex.ts script

---
+++ scripts/generate-merkle-root-non-hex.ts
@@ -0,0 +1,32 @@
+import { program } from 'commander'
+import fs from 'fs'
+import { parseBalanceMap } from '../src/parse-balance-map'
+
+program
+  .version('0.0.0')
+  .requiredOption(
+    '-i, --input <path>',
+    'input JSON file location containing a map of account addresses and balances'
+  )
+  .requiredOption(
+    '-i, --output <path>',
+    'Output JSON file location containing a map of account addresses and hex balances'
+  )
+
+program.parse(process.argv)
+
+let json = JSON.parse(fs.readFileSync(program.input, { encoding: 'utf8' }))
+
+if (typeof json !== 'object') throw new Error('Invalid JSON')
+
+let keys = Array.from( Object.keys(json) );
+
+for (let x = 0 ; x < keys.length ; x++ ){
+    json[String(keys[x])] = (json[String(keys[x])] * ( 10 ** 18 )).toString(16);
+}
+
+fs.writeFile (program.output, JSON.stringify(parseBalanceMap(json), null, 4), function(err) {
+    if (err) throw err;
+    console.log('complete');
+    }
+);
